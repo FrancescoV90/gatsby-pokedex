6be5cafac96ff70e938542079ef7f8223cb1b82b
Francesco Valli <francesco.valli@searchon.it>
2023-03-31 23:32:55 +0200

    init pokemon page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,30 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const NODE_TYPE = 'Pokemon';
+const NODE_TYPE = "Pokemon";
 
-exports.sourceNodes = async ({ actions, createContentDigest, createNodeId, getNodesByType }) => {
+exports.sourceNodes = async ({
+  actions,
+  createContentDigest,
+  createNodeId,
+  getNodesByType,
+}) => {
   const { createNode } = actions;
 
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=51");
   const json = await response.json();
   const { results = [] } = json;
 
-  const pokemon = await Promise.all(results.map(async result => {
-    const { url } = result;
-    const pokeResponse1 = await fetch(url);
-    const json1 = await pokeResponse1.json();
-    const { species } = json1;
-    const pokeResponse2 = await fetch(species.url);
-    const json2 = await pokeResponse2.json();
-    return {...json1, ...json2}
-  }));
+  const pokemon = await Promise.all(
+    results.map(async (result) => {
+      const { url } = result;
+      const pokeResponse1 = await fetch(url);
+      const json1 = await pokeResponse1.json();
+      const { species } = json1;
+      const pokeResponse2 = await fetch(species.url);
+      const json2 = await pokeResponse2.json();
+      return { ...json1, ...json2 };
+    })
+  );
 
   pokemon.forEach((node, index) => {
     return createNode({
@@ -28,10 +35,41 @@ exports.sourceNodes = async ({ actions, createContentDigest, createNodeId, getNo
       internal: {
         type: NODE_TYPE,
         content: JSON.stringify(node),
-        contentDigest: createContentDigest(node)
-      }
+        contentDigest: createContentDigest(node),
+      },
     });
   });
 
   return;
+};
+
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      allPokemon {
+        edges {
+          node {
+            id
+            name
+            types {
+              type {
+                name
+              }
+            }
+            sprites {
+              front_default
+            }
+          }
+        }
+      }
+    }
+  `);
+  data.allPokemon.edges.forEach((edge) => {
+    const slug = edge.node.name;
+    actions.createPage({
+      path: slug,
+      component: require.resolve(`./src/templates/pokemon-template.js`),
+      context: { slug: slug },
+    });
+  });
 };
---
+++ src/templates/pokemon-template.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import { graphql } from "gatsby";
+import Layout from "../components/layout";
+
+export default ({ data }) => {
+  //const pokemon = data.pokemon;
+  return (
+    <Layout>
+      <div>
+        <h1>provaaaaaaa</h1>
+      </div>
+    </Layout>
+  );
+};
+
+// export const query = graphql`
+//   query ($slug: String!) {
+//     pokemon(fields: { slug: { eq: $slug } }) {
+//       name
+//     }
+//   }
+// `;
