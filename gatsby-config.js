62781a3532a542c90977bba26fc83a82c2c0afab
Francesco Valli <francesco.valli@searchon.it>
2023-04-01 23:49:22 +0200

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: "Gatsby Pokedex",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Pokedex`,
+        short_name: `Gatsby Pokedex`,
+        start_url: `/`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link, useStaticQuery, graphql } from "gatsby";
+import { Link } from "gatsby";
 import {
   wrapper,
   main,
@@ -10,16 +10,6 @@ import {
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <div className={wrapper}>
       <header className={header}>
