6e9fd98e48ff1e3899e655423881896db430bf93
Francesco Valli <francesco.valli@searchon.it>
2023-04-04 14:07:27 +0200

    update pokemon template

--- src/templates/pokemon-template.js
+++ src/templates/pokemon-template.js
@@ -1,5 +1,6 @@
-import React from "react";
+import * as React from "react";
 import { graphql } from "gatsby";
+import { I18nextContext } from "gatsby-plugin-react-i18next";
 import Layout from "../components/layout";
 import {
   pokemonHeader,
@@ -10,30 +11,43 @@ import {
   pokemonContent,
 } from "./pokemon-template.module.css";
 
-const PokemonTemplate = ({ data, pageContext }) => {
-  console.log(pageContext);
-  const pokemon = data.allPokemon.nodes[0];
+const PokemonTemplate = ({ pageContext }) => {
+  const { language } = React.useContext(I18nextContext);
+  const pokemon = {
+    id: pageContext.id,
+    slug: pageContext.name,
+    name: pageContext.names.find((name) => name.language.name === language)
+      .name,
+    pokedex_number: pageContext.pokedex_numbers.find(
+      (pokedex) => pokedex.pokedex.name === "kanto"
+    ).entry_number,
+    image: pageContext.sprites.other.official_artwork.front_default,
+    genus: pageContext.genera.find((genus) => genus.language.name === language)
+      .genus,
+    description: pageContext.flavor_text_entries.find(
+      (description) => description.language.name === language
+    ).flavor_text,
+  };
+
   return (
     <Layout>
       <div className={pokemonHeader}>
         <h1 className={pokemonName}>{pokemon.name} N° </h1>
-        <span className={pokemonIndex}>
-          {pokemon.pokedex_numbers[0].entry_number}
-        </span>
+        <span className={pokemonIndex}>{pokemon.pokedex_number}</span>
       </div>
 
       <div className={pokemonWrapper}>
         <div className={pokemonImage}>
           <img
-            src={pokemon.sprites.other.official_artwork.front_default}
+            src={pokemon.image}
             alt={`${pokemon.name}`}
             width={475}
             height={475}
           />
         </div>
         <div className={pokemonContent}>
-          <h2>{pokemon.genera[7].genus}</h2>
-          <p>{pokemon.flavor_text_entries[0].flavor_text}</p>
+          <h2>{pokemon.genus}</h2>
+          <p>{pokemon.description}</p>
         </div>
       </div>
     </Layout>
@@ -41,7 +55,7 @@ const PokemonTemplate = ({ data, pageContext }) => {
 };
 
 export const query = graphql`
-  query ($name: String!, $language: String!) {
+  query ($language: String!) {
     locales: allLocale(
       filter: { ns: { in: ["index"] }, language: { eq: $language } }
     ) {
@@ -50,37 +64,6 @@ export const query = graphql`
           ns
           data
           language
-        }
-      }
-    }
-    allPokemon(filter: { name: { eq: $name } }) {
-      nodes {
-        id
-        name
-        sprites {
-          other {
-            official_artwork {
-              front_default
-            }
-          }
-        }
-        genera {
-          genus
-          language {
-            name
-          }
-        }
-        flavor_text_entries {
-          flavor_text
-          language {
-            name
-          }
-        }
-        pokedex_numbers {
-          entry_number
-          pokedex {
-            name
-          }
         }
       }
     }
